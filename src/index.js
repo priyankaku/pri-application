a1b2f565b23fccf765597a1c02486a9a77774ed3
Priyanka K U <priyanka.ku@gmail.com>
2020-03-30 12:49:14 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import Counter from './Parent';
+import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
